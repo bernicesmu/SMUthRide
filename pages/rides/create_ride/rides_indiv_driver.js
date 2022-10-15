14e226517379e801e8943510c4d03fa8855635e9
keithlyx <keith.lee.2021@scis.smu.edu.sg>
2022-10-16 02:57:30 +0800

    added date alert?
    will do the rest tmr

--- pages/rides/create_ride/rides_indiv_driver.js
+++ pages/rides/create_ride/rides_indiv_driver.js
@@ -1,2 +1,20 @@
 import {writeUserData} from '../../../index.js'
 writeUserData(1, username, "regine@hello.com", "regine.com")
+
+const ride_form = Vue.createApp({
+    data() {
+        return {
+            date: "",
+
+        }
+    },
+    methods:{
+        check_date(){
+            let selected_date = this.date.split("-")
+            selected_date = new Date(selected_date[0], selected_date[1], selected_date[2])
+            const today = new Date()
+            return selected_date - today >= 1000 * 60 * 60 * 24 * 365;
+        }
+    }
+})
+ride_form.mount('#ride_form')
