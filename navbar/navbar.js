a48451f066f985918fb0d9ae8ebeaf2120c1bfc5
Zhe_Xiang Chan <zxchan.2021@scis.smu.edu.sg>
2022-10-24 16:56:29 +0800

    add navbar to all pages

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -4,7 +4,6 @@ const navbar = Vue.createApp({
     data() {
         return {
             page: pageData,
-            activePage: "home",
         };
     },
     template: `
@@ -34,7 +33,7 @@ const navbar = Vue.createApp({
             >
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link nav-item-top" :class="{active: currentPage}" href="#">Home</a>
+                        <a class="nav-link nav-item-top" href="#">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link nav-item-top" href="/pages/rides/rides_list/listing.html">Rides</a>
@@ -48,26 +47,17 @@ const navbar = Vue.createApp({
                     <li class="nav-item">
                         <a class="nav-link nav-item-top" href="/pages/profile/profile.html"><img src="/pages/profile/ded.png" class="profile-img"/></a>
                     </li>
-                    
                 </ul>
             </div>
         </nav>
     `,
-    computed: {
-        currentPage() {
-            if (this.page === this.activePage) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-    },
+    // computed: {
+    //     currentPage() {
+    //         innerText.toLowerCase() === this.page;
+    //     },
+    // },
     methods: {
         nav_animation() {
-            // console.log("nav animation working");
-            // console.log(
-            //     document.querySelectorAll("#navbarSupportedContent li")
-            // );
             let currentScrollPos = window.pageYOffset;
             if (currentScrollPos == 0) {
                 document
@@ -96,6 +86,11 @@ const navbar = Vue.createApp({
     },
     mounted() {
         window.addEventListener("scroll", this.nav_animation);
+        for (const elem of document.getElementsByClassName("nav-link")) {
+            if (elem.innerText.toLowerCase() === this.page) {
+                elem.classList.add("active-page");
+            }
+        }
     },
 });
 
