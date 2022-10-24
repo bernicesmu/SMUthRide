4f8a27981fea39700627934d36ebd09b3509ccd9
keithlyx <keith.lee.2021@scis.smu.edu.sg>
2022-10-24 17:27:14 +0800

    naming

--- pages/offers/offers.js
+++ pages/offers/offers.js
@@ -1,4 +1,3 @@
-
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 
@@ -19,7 +18,6 @@ const db = getDatabase();
 const listings = Vue.createApp({
     data() {
         return{
-            users: [],
             listings: [],
             to_from : "From",
             display_listings: [],
@@ -31,17 +29,17 @@ const listings = Vue.createApp({
     methods: {
         change_direction(){
             this.to_from = this.to_from === "To" ? "From" : "To";
+            console.log(this.to_from)
             this.check_and_populate()
         },
         check_and_populate(){
             if (this.to_from === "To"){
-                this.display_listings = this.listings.filter(x => x.smu_to_from === "To");
+                this.display_listings = this.listings.filter(x => x.smu_pick_or_drop === "drop");
             } else if (this.to_from === "From"){
-                this.display_listings = this.listings.filter(x => x.smu_to_from === "From");
+                this.display_listings = this.listings.filter(x => x.smu_pick_or_drop === "pick");
             }
-
         },
-        formatAMPM(date) {
+        formatAMPM(date){
 
             let hours = Number(date.split(":")[0]);
             let minutes = Number(date.split(":")[1]);
@@ -56,10 +54,6 @@ const listings = Vue.createApp({
             date = date.split("-")
             let day = new Date(date[0], date[1], date[2]).toDateString().split(" ")
             return [day[0],`${day[1]} ${day[2]} ${day[3]}`]
-        },
-        get_user_name(id){
-            let user = this.users.filter(x => x.id === id)
-            return user[0].name
         }
     },
     mounted() {
@@ -67,19 +61,13 @@ const listings = Vue.createApp({
         const db = getDatabase();
         const rides = ref(db, `rides/`)
         const users = ref(db, `users/`)
-        onValue(users, (snapshot) => {
-            this.users = snapshot.val();
-            console.log(this.users)
-        });
-
         onValue(rides, (snapshot) => {
 
             this.listings = snapshot.val()
             this.listings.splice(0, 1)
-
             this.check_and_populate()
         })
-        }
+    }
 })
 
 listings.mount('#populate_listings')
