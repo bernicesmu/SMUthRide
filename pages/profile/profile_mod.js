c24018e764e2e123d1c330911f352e4fd70c9677
Zhe_Xiang Chan <zxchan.2021@scis.smu.edu.sg>
2022-10-25 17:23:04 +0800

    Updated cca edit functionality

--- pages/profile/profile_mod.js
+++ pages/profile/profile_mod.js
@@ -1,34 +1,59 @@
-import { find_name_from_username, find_user_profile, write_user_profile } from "../../index.js";
+import {
+    find_name_from_username,
+    find_user_profile,
+    write_user_profile,
+} from "../../index.js";
 
-var username = localStorage.getItem("username_x")
+var username = localStorage.getItem("username_x");
 
-document.getElementById("profile_form").addEventListener("submit", update_user_database)
+find_user_profile(username);
+find_name_from_username(username);
 
-find_user_profile(username)
-find_name_from_username(username)
+let elem = document.getElementById("profile_form");
+if (elem) {
+    elem.addEventListener("submit", update_user_database);
+}
 
-var displayname = localStorage.getItem("displayname")
- 
-function update_user_database() { 
-    var inputs = document.getElementsByClassName("target-input")
-    console.log(inputs)
+var displayname = localStorage.getItem("displayname");
 
-    var displayname = inputs.displayname.value
-    var age = inputs.age.value
-    var bio = inputs.bio.value
-    var cca = ['ellipsis', 'smubia', 'gourmet']
-    var comfort = inputs.prefComfort.value
-    var convenience = inputs.prefConvenience.value
-    var degree = inputs.degree.value
-    var facebook = inputs.facebook.value
-    var instagram = inputs.instagram.value
-    var linkedin = inputs.linkedin.value
-    var location_user = "Jurong" //hardcoded. softcode after regine is done
-    var mbti = inputs.mbti.value
-    var price = inputs.prefPrice.value
-    var speed = inputs.prefSpeed.value
-    var rs_status = inputs.status.value
-    var year = inputs.year.value
-    
-    write_user_profile(username, displayname, age, bio, cca, comfort, convenience, degree, facebook, instagram, linkedin, location_user, mbti, price, speed, rs_status, year)
+function update_user_database() {
+    var inputs = document.getElementsByClassName("target-input");
+    console.log(inputs);
+
+    var displayname = inputs.displayname.value;
+    var age = inputs.age.value;
+    var bio = inputs.bio.value;
+    var cca = ["ellipsis", "smubia", "gourmet"];
+    var comfort = inputs.prefComfort.value;
+    var convenience = inputs.prefConvenience.value;
+    var degree = inputs.degree.value;
+    var facebook = inputs.facebook.value;
+    var instagram = inputs.instagram.value;
+    var linkedin = inputs.linkedin.value;
+    var location_user = "Jurong"; //hardcoded. softcode after regine is done
+    var mbti = inputs.mbti.value;
+    var price = inputs.prefPrice.value;
+    var speed = inputs.prefSpeed.value;
+    var rs_status = inputs.status.value;
+    var year = inputs.year.value;
+
+    write_user_profile(
+        username,
+        displayname,
+        age,
+        bio,
+        cca,
+        comfort,
+        convenience,
+        degree,
+        facebook,
+        instagram,
+        linkedin,
+        location_user,
+        mbti,
+        price,
+        speed,
+        rs_status,
+        year
+    );
 }
