8fd75346b2e48e14297e5bae0048acd2e2a9e2c7
bernicesmu <bernice.teo.2021@SCIS.SMU.EDU.sg>
2022-10-28 04:38:04 +0800

    date time for rides_indiv_rider

--- pages/rides/ride_details/rides_indiv_rider.js
+++ pages/rides/ride_details/rides_indiv_rider.js
@@ -1,4 +1,4 @@
-import { get_ride_details, find_user_profile, find_name_from_username } from "../../../index.js";
+import { get_ride_details, find_user_profile, find_name_from_username, format_date, formatAMPM } from "../../../index.js";
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -18,9 +18,9 @@ document.getElementById("costperpax").innerHTML = `$${cost} per pax`
 var max_capacity = localStorage.getItem("max_capacity")
 var capacity = localStorage.getItem("users_offered").split(",").length
 document.getElementById("capacity").innerHTML = `${capacity} / ${max_capacity} seats available`
-var date = localStorage.getItem("date")
-var time = localStorage.getItem("time")
-document.getElementById("date_time").innerHTML = `${date}, ${time}`
+var date = format_date(localStorage.getItem("date"))
+var time = formatAMPM(localStorage.getItem("time"))
+document.getElementById("date_time").innerHTML = `${date[1]} (${date[0]}), ${time}`
 
 var username = localStorage.getItem("username_x")
 localStorage.clear()
