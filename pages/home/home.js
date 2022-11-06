b26ed8cb6b2557f4b6ede625fdbc36991003181e
oreoanytime <111495669+oreoanytime@users.noreply.github.com>
2022-11-06 10:55:37 +0800

    home page changes

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,5 @@
+// const { TabsPlugin } = require("bootstrap-vue");
+
 const pageData = document.querySelector("#navbarVue div").dataset.page;
 
 const navbar = Vue.createApp({
@@ -104,3 +106,25 @@ const navbar = Vue.createApp({
 });
 
 navbar.mount("#navbarVue");
+
+
+document.getElementById("swap").addEventListener("click", swap_to_frm)
+
+function swap_to_frm() {
+    var from  = document.getElementById("from")
+    var to = document.getElementById("to")
+    var sen = document.getElementById("trip-sen")
+
+    if (from.innerHTML == "SMU") {
+        from.innerHTML = "Location"
+        to.innerHTML = "SMU"
+        sen.innerHTML = "Booking a ride to SMU"
+    }
+    else {
+        from.innerHTML = "SMU"
+        to.innerHTML = "Location"
+        sen.innerHTML = "Booking a ride from SMU"
+    }
+
+
+}
