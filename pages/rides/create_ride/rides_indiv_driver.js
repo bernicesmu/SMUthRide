71ec72aa2cdd8d1b2b87a6b380a22946345143c7
bernicesmu <bernice.teo.2021@SCIS.SMU.EDU.sg>
2022-11-12 04:34:11 +0800

    rides indiv driver time and design

--- pages/rides/create_ride/rides_indiv_driver.js
+++ pages/rides/create_ride/rides_indiv_driver.js
@@ -63,6 +63,67 @@ const form_alerts = Vue.createApp({
     computed:{
        
     }
+});
+
+form_alerts.component('time-input', { 
+    data() { 
+        return { 
+
+        }
+    },
+
+    template: ` <div class='time-input'> 
+                <select
+                    name="time_hour"
+                    id="time_hour"
+                    class="dropdowns dropdown-time"
+                >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                </select>
+                :
+                <select
+                    name="time_min"
+                    id="time_min"
+                    class="dropdowns dropdown-time"
+                >
+                    <option value='00'>00</option>
+                    <option value='05'>05</option>
+                    <option value='10'>10</option>
+                    <option value='15'>15</option>
+                    <option value='20'>20</option>
+                    <option value='25'>25</option>
+                    <option value='30'>30</option>
+                    <option value='35'>35</option>
+                    <option value='40'>40</option>
+                    <option value='45'>45</option>
+                    <option value='50'>50</option>
+                    <option value='55'>55</option>
+                </select>
+                &nbsp;
+                <select
+                    name="time_ampm"
+                    id="time_ampm"
+                    class="dropdowns dropdown-time"
+                >
+                    <option value="am">AM</option>
+                    <option value="pm">PM</option>
+                </select>
+                </div>`,
+
+    methods: { 
+
+    }
 })
 
 form_alerts.mount('#form_alerts')
@@ -86,11 +147,16 @@ async function write_ride_local() {
     var cost = parseFloat(inputs.cost.value)
     var max_capacity = parseInt(inputs.capacity.value)
     var date = inputs.date.value
-    var time = inputs.time.value 
     var users_offered = [""]
     var area = document.getElementById("hidden_formatted_address").value
     var neighbourhood = document.getElementById("hidden_neighbourhood").value 
-    
+    var time_hour = document.getElementById("time_hour").value 
+    var time_min = document.getElementById("time_min").value
+    var time_ampm = document.getElementById("time_ampm").value
+    if (time_ampm == "pm") { 
+        time_hour = parseInt(time_hour) + 12 
+    }
+    var time = String(time_hour) + ":" + time_min
 
     let check = await write_ride(smu_location,smu_position,username,rideid,user_address,cost,max_capacity,date,time,users_offered,area,neighbourhood)
     console.log(check)
